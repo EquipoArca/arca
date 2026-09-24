a3cb6ceaf11c00b30c583f648c5c118bddcf8fc2
equipoarca <arcaoficial0@gmail.com>
2026-09-24 15:14:10 -0500

    Cambiar nombre

--- public/perfil.js
+++ public/perfil.js
@@ -1,6 +1,5 @@
-// ==========================================
-// FUNCIÓN PARA COMPRIMIR Y CONVERTIR A BASE64
-// ==========================================
+import emailjs from 'https://cdn.jsdelivr.net/npm/@emailjs/browser@4/+esm';
+emailjs.init("AVpXUrYdpwa9oh438");
 const convertirFotoPerfilBase64 = (archivo) => {
     return new Promise((resolve, reject) => {
         const reader = new FileReader();
